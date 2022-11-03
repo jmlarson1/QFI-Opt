126b4aaba14f6226afe0e8fa71ad3735db421c8c
Michael A. Perlin <mika.perlin@gmail.com>
2022-11-03 16:07:26 -0500

    initial check script commit

---
+++ check/all_.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import sys
+import textwrap
+
+import check_utils
+import flake8_
+import format_
+import mypy_
+
+
+def run(*args: str) -> int:
+
+    parser = check_utils.get_file_parser()
+    parser.description = textwrap.dedent(
+        """
+        Runs all checks on the repository.
+        Exits immediately upon any failure unless passed -f or --force as an argument.
+        This script exits with a succeeding exit code if and only if all checks pass.
+        """
+    )
+
+    parser.add_argument("-f", "--force", action="store_true", help="Continue past (i.e., do not exit after) failing checks.")
+
+    parsed_args = parser.parse_args(args)
+    exit_on_failure = not parsed_args.force
+
+    checks_failed = 0
+    checks_failed |= format_.run(*parsed_args.files, exit_on_failure=exit_on_failure, silent=False)
+    checks_failed |= flake8_.run(*parsed_args.files, exit_on_failure=exit_on_failure, silent=True)
+    checks_failed |= mypy_.run(*parsed_args.files, exit_on_failure=exit_on_failure, silent=True)
+
+    return checks_failed
+
+
+if __name__ == "__main__":
+    exit(run(*sys.argv[1:]))
---
+++ check/flake8_.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+import textwrap
+from typing import Iterable
+
+import check_utils
+
+
+@check_utils.enable_exit_on_failure
+def run(
+    *args: str,
+    include: str | Iterable[str] = "*.py",
+    exclude: str | Iterable[str] = "",
+    silent: bool = False,
+) -> int:
+
+    parser = check_utils.get_file_parser()
+    parser.description = textwrap.dedent(
+        """
+        Runs flake8 on the repository (formatting check).
+        """
+    )
+
+    _, args_to_pass = parser.parse_known_intermixed_args(args)
+    files = check_utils.get_tracked_files(include, exclude)
+
+    return subprocess.call(["flake8", *files, *args_to_pass], cwd=check_utils.root_dir)
+
+
+if __name__ == "__main__":
+    exit(run(*sys.argv[1:]))
