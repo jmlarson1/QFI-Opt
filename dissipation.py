702ffc9920085cce993b3b560fa1c328c1c49813
Michael A. Perlin <mika.perlin@gmail.com>
2023-01-31 14:39:49 -0600

    minox fixes to dissipation.py

--- dissipation.py
+++ dissipation.py
@@ -37,32 +37,32 @@ class Dissipator:
 
         if dissipation_format == "XYZ":
             rate_sx, rate_sy, rate_sz = dissipation_rates
-            self._rate_0 = (rate_sx + rate_sy) / 4 + rate_sz / 2
-            self._rate_1 = (rate_sx + rate_sy) / 4
-            self._rate_2 = (rate_sx - rate_sy) / 4
-            self._qubit_term_0 = _qubit_term_XYZ_1
-            self._qubit_term_1 = _qubit_term_XYZ_2
-            self._qubit_term_2 = _qubit_term_XYZ_3
+            self._rate_1 = (rate_sx + rate_sy) / 4 + rate_sz / 2
+            self._rate_2 = (rate_sx + rate_sy) / 4
+            self._rate_3 = (rate_sx - rate_sy) / 4
+            self._qubit_term_1 = _qubit_term_XYZ_1
+            self._qubit_term_2 = _qubit_term_XYZ_2
+            self._qubit_term_3 = _qubit_term_XYZ_3
 
         elif dissipation_format == "PMZ":
             rate_sp, rate_sm, rate_sz = dissipation_rates
-            self._rate_0 = sum(dissipation_rates) / 2
-            self._rate_1 = rate_sp
-            self._rate_2 = rate_sm
-            self._qubit_term_0 = _qubit_term_PMZ_1
-            self._qubit_term_1 = _qubit_term_PMZ_2
-            self._qubit_term_2 = _qubit_term_PMZ_3
+            self._rate_1 = sum(dissipation_rates) / 2
+            self._rate_2 = rate_sp
+            self._rate_3 = rate_sm
+            self._qubit_term_1 = _qubit_term_PMZ_1
+            self._qubit_term_2 = _qubit_term_PMZ_2
+            self._qubit_term_3 = _qubit_term_PMZ_3
 
         else:
-            raise ValueError(f"dissipation format not recognized {dissipation_format}")
+            raise ValueError(f"dissipation format not recognized: {dissipation_format}")
 
     def __matmul__(self, density_op: np.ndarray) -> np.ndarray:
         num_qubits = log2_int(density_op.size) // 2
-        output = self._rate_0 * sum((self._qubit_term_0(density_op, qubit) for qubit in range(num_qubits)), start=np.array(0))
-        if self._rate_1:
-            output += self._rate_1 * sum(self._qubit_term_1(density_op, qubit) for qubit in range(num_qubits))
+        output = self._rate_1 * sum((self._qubit_term_1(density_op, qubit) for qubit in range(num_qubits)), start=np.array(0))
         if self._rate_2:
             output += self._rate_2 * sum(self._qubit_term_2(density_op, qubit) for qubit in range(num_qubits))
+        if self._rate_3:
+            output += self._rate_3 * sum(self._qubit_term_3(density_op, qubit) for qubit in range(num_qubits))
         return output
 
     def __mul__(self, scalar: float) -> "Dissipator":
