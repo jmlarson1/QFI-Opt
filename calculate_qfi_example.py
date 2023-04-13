4b8c715f0aba512e7eb7a10ad353de8be0cfe5fe
Jeffrey Larson <jmlarson@anl.gov>
2023-04-13 11:37:43 -0500

    Error in spin_chain simulation

--- calculate_qfi_example.py
+++ calculate_qfi_example.py
@@ -36,18 +36,28 @@ if __name__ == "__main__":
     dissipation = 0
     G = spin_models.collective_op(spin_models.PAULI_Z, N) / (2 * N)
 
-    # Let's try calculating the QFI at all corner points of the domain:
-    all_perms = [",".join(seq) for seq in itertools.product("01", repeat=4)]
-    for perm in all_perms:
-        params = np.fromstring(perm, dtype=int, sep=",")
-        rho = spin_models.simulate_OAT(params, N, dissipation)
-        qfi = compute_QFI(rho, G)
-        print(f"QFI is {qfi} for {params}")
+    # # Let's try calculating the QFI at all corner points of the domain:
+    # all_perms = [",".join(seq) for seq in itertools.product("01", repeat=4)]
+    # for perm in all_perms:
+    #     params = np.fromstring(perm, dtype=int, sep=",")
+    #     rho = spin_models.simulate_OAT(params, N, dissipation)
+    #     qfi = compute_QFI(rho, G)
+    #     print(f"QFI is {qfi} for {params}")
 
-    # Let's try calculating the QFI at some random points in the domain:
-    np.random.seed(0)
-    for _ in range(10):
-        params = np.random.uniform(0, 1, 4)
-        rho = spin_models.simulate_OAT(params, N, dissipation)
-        qfi = compute_QFI(rho, G)
-        print(f"QFI is {qfi} for {params}")
+    num_rand_pts = 2
+    # Calculate QFI for models at random points in the domain.
+    for num_params in [4, 5]:
+        match num_params:
+            case 4:
+                models = ['simulate_OAT', 'simulate_ising_chain', 'simulate_XX_chain']
+            case 5:
+                models = ['simulate_TAT','simulate_spin_chain','simulate_local_TAT_chain']
+
+        for model in models:
+            np.random.seed(0)
+            obj = getattr(spin_models, model)
+            for _ in range(num_rand_pts):
+                params = np.random.uniform(0, 1, num_params)
+                rho = obj(params, N, dissipation)
+                qfi = compute_QFI(rho, G)
+                print(f"QFI is {qfi} for {params}")
