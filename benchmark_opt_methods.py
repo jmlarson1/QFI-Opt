9e2fb55a37cb7f740d765c2070125549acdce3c0
Jeffrey Larson <jmlarson@anl.gov>
2023-07-18 14:28:50 -0500

    Parallelising

--- benchmark_opt_methods.py
+++ benchmark_opt_methods.py
@@ -1,44 +1,48 @@
 import os
 import sys
+import pickle
 
 import matplotlib.pyplot as plt
 import nlopt
 import numpy as np
 
+from mpi4py import MPI
 import spin_models
 from calculate_qfi_example import compute_QFI
 
-sys.path.append("/home/jlarson/research/poptus/orbit/py")
-sys.path.append("/home/jlarson/research/poptus/minq/py/minq5/")
-from ibcdfo.pounders import pounders
+sys.path.append("/home/jmlarson/research/poptus/orbit/py")
+sys.path.append("/home/jmlarson/research/poptus/minq/py/minq5/")
+
 from ibcdfo.pounders.general_h_funs import identity_combine as combinemodels
 from orbit4py import ORBIT2
+import ibcdfo.pounders.pounders as pounders
 
 
 def nlopt_wrapper(x, grad, obj, obj_params):
-    global all_f
+    global all_f, all_X
     database = obj.__name__ + "_" + str(obj_params["N"]) + "_" + str(obj_params["dissipation"]) + "_database.npy"
     DB = []
     match = 0
-    if os.path.exists(database):
-        DB = np.load(database, allow_pickle=True)
-        for db_entry in DB:
-            if np.allclose(db_entry["var_vals"], x, rtol=1e-12, atol=1e-12):
-                rho = db_entry["rho"]
-                match = 1
-                break
+    # if os.path.exists(database):
+    #     DB = np.load(database, allow_pickle=True)
+    #     for db_entry in DB:
+    #         if np.allclose(db_entry["var_vals"], x, rtol=1e-12, atol=1e-12):
+    #             rho = db_entry["rho"]
+    #             match = 1
+    #             break
 
     if match == 0:
         # Do the sim
         rho = obj(x, obj_params["N"], dissipation_rates=obj_params["dissipation"])
 
-        to_save = {"rho": rho, "var_vals": x}
-        DB = np.append(DB, to_save)
-        np.save(database, DB)
+        # to_save = {"rho": rho, "var_vals": x}
+        # DB = np.append(DB, to_save)
+        # np.save(database, DB)
 
     qfi = compute_QFI(rho, obj_params["G"])
-    print(x, qfi, flush=True)
+    # print(x, qfi, flush=True)
     all_f.append(qfi)
+    all_X.append(x)
     return -1 * qfi  # negative because we are maximizing
 
 
@@ -63,6 +67,11 @@ def run_orbit(obj, obj_params, n, x0):
         calfun, rbftype, gamma_m, n, max_evals, npmax, delta, maxdelta, trnorm, gtol, Low, Upp, nfs, X, F, xkin
     )
 
+    # print("optimum at ", X[xkin])
+    # print("minimum value = ", F[xkin])
+
+    return F[xkin], X[xkin] 
+
 
 def run_pounder(obj, obj_params, n, x0):
     calfun = lambda x: nlopt_wrapper(x, [], obj, obj_params)
@@ -74,7 +83,7 @@ def run_pounder(obj, obj_params, n, x0):
     delta = 0.1
     m = 1
     nfs = 1
-    printf = True
+    printf = False
     spsolver = 2
     gtol = 1e-9
     xind = 0
@@ -82,6 +91,10 @@ def run_pounder(obj, obj_params, n, x0):
 
     [X, F, flag, xkin] = pounders(calfun, X, n, mpmax, max_evals, gtol, delta, nfs, m, F, xind, Low, Upp, printf, spsolver, hfun, combinemodels)
 
+    # print("optimum at ", X[xkin])
+    # print("minimum value = ", F[xkin])
+
+    return F[xkin], X[xkin] 
 
 def run_nlopt(obj, obj_params, num_params, x0, solver):
     opt = nlopt.opt(getattr(nlopt, solver), num_params)
@@ -98,12 +111,19 @@ def run_nlopt(obj, obj_params, num_params, x0, solver):
 
     x = opt.optimize(x0)
     minf = opt.last_optimum_value()
-    print("optimum at ", x)
-    print("minimum value = ", minf)
-    print("result code = ", opt.last_optimize_result())
+    # print("optimum at ", x)
+    # print("minimum value = ", minf)
+    # print("result code = ", opt.last_optimize_result())
+
+    return minf, x
 
 
 if __name__ == "__main__":
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
     N = 4
     G = spin_models.collective_op(spin_models.PAULI_Z, N) / (2 * N)
 
@@ -113,55 +133,81 @@ if __name__ == "__main__":
         obj_params["dissipation"] = d
         obj_params["G"] = G
 
-        max_evals = 100
+        max_evals = 500
 
         for num_params in [4, 5]:
             lb = np.zeros(num_params)
             ub = np.ones(num_params)
 
-            for seed in [0, 1]:
-                # x0 = 0.5 * np.ones(num_params)  # This is an optimum for the num_params==4 problems
-                np.random.seed(seed)
-                x0 = np.random.uniform(lb, ub, num_params)
+            for seed in range(size):
+                if seed % size == rank: 
+                    # x0 = 0.5 * np.ones(num_params)  # This is an optimum for the num_params==4 problems
+                    np.random.seed(seed)
+                    x0 = np.random.uniform(lb, ub, num_params)
 
-                match num_params:
-                    case 4:
-                        models = ["simulate_OAT", "simulate_ising_chain", "simulate_XX_chain"]
-                    case 5:
-                        models = ["simulate_TAT", "simulate_local_TAT_chain"]
+                    match num_params:
+                        case 4:
+                            models = ["simulate_OAT", "simulate_ising_chain", "simulate_XX_chain"]
+                            # models = ["simulate_OAT"]
+                        case 5:
+                            models = ["simulate_TAT", "simulate_local_TAT_chain"]
+                            # models = ["simulate_TAT"]
 
-                for model in models:
-                    print(model)
-                    fig_filename = "Results_" + model + "_" + str(d) + "_" + str(seed)
-                    if os.path.exists(fig_filename + ".png"):
-                        continue
-                    obj = getattr(spin_models, model)
+                    for model in models:
+                        filename = model + '_' + str(d) + '_'  + str(seed) + '.pkl'
+                        fig_filename = "Results_" + model + "_" + str(d) + "_" + str(seed)
+                        if os.path.exists(filename):
+                            continue
+                        if os.path.exists(fig_filename + ".png"):
+                            continue
+                        obj = getattr(spin_models, model)
 
-                    for solver in ["LN_NELDERMEAD", "LN_BOBYQA", "ORBIT", "POUNDER"]:
-                        global all_f
-                        all_f = []
-                        if solver in ["LN_NELDERMEAD", "LN_BOBYQA"]:
-                            run_nlopt(obj, obj_params, num_params, x0, solver)
-                        elif solver in ["ORBIT"]:
-                            run_orbit(obj, obj_params, num_params, x0)
-                        elif solver in ["POUNDER"]:
-                            run_pounder(obj, obj_params, num_params, x0)
+                        best_val = {}
+                        best_pt = {}
+                        for solver in ["LN_NELDERMEAD", "LN_BOBYQA", "ORBIT", "POUNDER"]:
+                            global all_f, all_X
+                            all_f = []
+                            all_X = []
+                            if solver in ["LN_NELDERMEAD", "LN_BOBYQA"]:
+                                run_nlopt(obj, obj_params, num_params, x0, solver)
+                            elif solver in ["ORBIT"]:
+                                run_orbit(obj, obj_params, num_params, x0)
+                            elif solver in ["POUNDER"]:
+                                run_pounder(obj, obj_params, num_params, x0)
 
-                        plt.figure(fig_filename)
-                        plt.plot(all_f, label=solver)
+                            print(all_f, all_X, flush=True)
 
-                        for i in range(1, len(all_f)):
-                            all_f[i] = max(all_f[i - 1], all_f[i])
+                            ind = np.argmax(all_f)
+                            best_val[solver] = all_f[ind]
+                            best_pt[solver] = all_X[ind]
 
-                        plt.figure(fig_filename + "best")
-                        plt.plot(all_f, label=solver)
+                        best_method = max(best_val, key=best_val.get)
+                        # print(model, d, seed, best_val[best_method], best_pt[best_method], flush=True)
+                        dic = {}
+                        dic["best_method"] = best_method
+                        dic["best_val"] = best_val[best_method]
+                        dic["best_pt"] = best_pt[best_method]
 
-                    plt.figure(fig_filename)
-                    plt.legend()
-                    plt.title(fig_filename)
-                    plt.savefig(fig_filename + ".png", dpi=300)
+                        with open(filename, 'wb') as f:
+                            pickle.dump(dic, f)
 
-                    plt.figure(fig_filename + "best")
-                    plt.legend()
-                    plt.title(fig_filename)
-                    plt.savefig(fig_filename + "best" + ".png", dpi=300)
+                        sys.exit('a')
+
+                        #     plt.figure(fig_filename)
+                        #     plt.plot(all_f, label=solver)
+
+                        #     for i in range(1, len(all_f)):
+                        #         all_f[i] = max(all_f[i - 1], all_f[i])
+
+                        #     plt.figure(fig_filename + "best")
+                        #     plt.plot(all_f, label=solver)
+
+                        # plt.figure(fig_filename)
+                        # plt.legend()
+                        # plt.title(fig_filename)
+                        # plt.savefig(fig_filename + ".png", dpi=300)
+
+                        # plt.figure(fig_filename + "best")
+                        # plt.legend()
+                        # plt.title(fig_filename)
+                        # plt.savefig(fig_filename + "best" + ".png", dpi=300)
