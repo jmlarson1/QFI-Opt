80465bed4f38af5e920fdb20e9d26bcb74b58e72
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2023-08-30 14:57:01 -0500

    JAX version of compute_QFI with derivatives

--- qfi_opt/examples/calculate_qfi.py
+++ qfi_opt/examples/calculate_qfi.py
@@ -3,6 +3,9 @@ import numpy as np
 
 from qfi_opt import spin_models
 
+import jax
+import jax.numpy as jnp
+jax.config.update("jax_enable_x64", True)
 
 def variance(rho: np.ndarray, G: np.ndarray) -> float:
     """Variance of self-adjoint operator (observable) G in the state rho."""
@@ -16,22 +19,23 @@ def compute_eigendecompotion(rho: np.ndarray):
     return eigvals, eigvecs
 
 
-def compute_QFI(eigvals: np.ndarray, eigvecs: np.ndarray, G: np.ndarray, tol: float = 1e-8) -> float:
+def compute_QFI(eigvals: jnp.ndarray, eigvecs: jnp.ndarray, G: jnp.ndarray, tol: float = 1e-8) -> jnp.ndarray:
     # Note: The eigenvectors must be rows of eigvecs
     num_vals = len(eigvals)
 
     # Compute QFI
-    running_sum = 0
+    running_sum = jnp.array(0)
     for i in range(num_vals):
         for j in range(i + 1, num_vals):
             denom = eigvals[i] + eigvals[j]
-            if not np.isclose(denom, 0, atol=tol):
+            if not jnp.isclose(denom, 0, atol=tol):
                 numer = (eigvals[i] - eigvals[j]) ** 2
                 term = eigvecs[i].conj() @ G @ eigvecs[j]
-                running_sum += numer / denom * np.linalg.norm(term) ** 2
+                running_sum += numer / denom * jnp.linalg.norm(term) ** 2
 
     return 4 * running_sum
 
+jacrev_compute_QFI = jax.jacrev(compute_QFI, argnums=(0,1))
 
 if __name__ == "__main__":
     num_spins = 4
@@ -57,15 +61,21 @@ if __name__ == "__main__":
             vals, vecs = compute_eigendecompotion(rho)
             qfi = compute_QFI(vals, vecs, op)
             print(f"QFI is {qfi} for {params}")
+            jac = jacrev_compute_QFI(vals, vecs, op)
+            print(f"Jacobian is {jac}")
 
             params[-1] = 0.0
             rho = obj(params, num_spins, dissipation_rates=dissipation)
             vals, vecs = compute_eigendecompotion(rho)
             qfi = compute_QFI(vals, vecs, op)
             print(f"QFI is {qfi} for {params}")
+            jac = jacrev_compute_QFI(vals, vecs, op)
+            print(f"Jacobian is {jac}")
 
             params[-1] = 1.0
             rho = obj(params, num_spins, dissipation_rates=dissipation)
             vals, vecs = compute_eigendecompotion(rho)
             qfi = compute_QFI(vals, vecs, op)
             print(f"QFI is {qfi} for {params}")
+            jac = jacrev_compute_QFI(vals, vecs, op)
+            print(f"Jacobian is {jac}")
