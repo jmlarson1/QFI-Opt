8ea8d7ead55895f535dfad3ab312953ee6b710d4
Jeffrey Larson <jmlarson@anl.gov>
2023-09-08 14:48:38 -0500

    Ensuring no negative QFI denominators

--- qfi_opt/examples/calculate_qfi.py
+++ qfi_opt/examples/calculate_qfi.py
@@ -16,16 +16,27 @@ def compute_eigendecompotion(rho: np.ndarray):
     return eigvals, eigvecs
 
 
-def compute_QFI(eigvals: np.ndarray, eigvecs: np.ndarray, G: np.ndarray, tol: float = 1e-8) -> float:
+def compute_QFI(eigvals: np.ndarray, eigvecs: np.ndarray, G: np.ndarray) -> float:
     # Note: The eigenvectors must be rows of eigvecs
     num_vals = len(eigvals)
+
+    # There should never be negative eigenvalues, so their magnitude gives an
+    # empirical estimate of the numerical accuracy of the eigendecomposition.
+    # We discard any QFI terms denominators within an order of magnitude of
+    # this value. 
+    low = np.min(eigvals)
+
+    if low < 0:
+        tol = -10 * np.min(eigvals)
+    else: 
+        tol = 1e-8
 
     # Compute QFI
     running_sum = 0
     for i in range(num_vals):
         for j in range(i + 1, num_vals):
             denom = eigvals[i] + eigvals[j]
-            if not np.isclose(denom, 0, atol=tol):
+            if not np.isclose(denom, 0, atol=tol, rtol=tol):
                 numer = (eigvals[i] - eigvals[j]) ** 2
                 term = eigvecs[i].conj() @ G @ eigvecs[j]
                 running_sum += numer / denom * np.linalg.norm(term) ** 2
