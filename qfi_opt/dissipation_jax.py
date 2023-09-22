42311e105e1942822f3f4d51381b3b1aff4163d1
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-22 13:37:38 -0500

    add jax/numpy dissipation.py files

--- qfi_opt/dissipation_jax.py
+++ qfi_opt/dissipation_jax.py
@@ -28,46 +28,50 @@ class Dissipator:
         if not isinstance(dissipation_rates, tuple):
             dissipation_rates = (dissipation_rates,) * 3
         assert all(rate >= 0 for rate in dissipation_rates), "dissipation rates cannot be negative!"
-        self._rates = dissipation_rates
+        self._bare_rates = dissipation_rates
         self._format = dissipation_format
 
         # This dissipator acts on a density matrix as
         #   dissipator @ rho = sum_j rate_j sum(qubit_term_j(rho, qubit) for qubit in all_qubits).
         # Identify the rates and qubit_term functions to use below.
 
+        rates = [0.0, 0.0, 0.0]
         if dissipation_format == "XYZ":
             rate_sx, rate_sy, rate_sz = dissipation_rates
-            self._rate_1 = (rate_sx + rate_sy) / 4 + rate_sz / 2
-            self._rate_2 = (rate_sx + rate_sy) / 4
-            self._rate_3 = (rate_sx - rate_sy) / 4
-            self._qubit_term_1 = _qubit_term_XYZ_1
-            self._qubit_term_2 = _qubit_term_XYZ_2
-            self._qubit_term_3 = _qubit_term_XYZ_3
+            rates[0] = (rate_sx + rate_sy) / 4 + rate_sz / 2
+            rates[1] = (rate_sx + rate_sy) / 4
+            rates[2] = (rate_sx - rate_sy) / 4
+            terms = (_qubit_term_XYZ_1, _qubit_term_XYZ_2, _qubit_term_XYZ_3)
 
         elif dissipation_format == "PMZ":
             rate_sp, rate_sm, rate_sz = dissipation_rates
-            self._rate_1 = sum(dissipation_rates) / 2
-            self._rate_2 = rate_sp
-            self._rate_3 = rate_sm
-            self._qubit_term_1 = _qubit_term_PMZ_1
-            self._qubit_term_2 = _qubit_term_PMZ_2
-            self._qubit_term_3 = _qubit_term_PMZ_3
+            rates[0] = sum(dissipation_rates) / 2
+            rates[1] = rate_sp
+            rates[2] = rate_sm
+            terms = (_qubit_term_PMZ_1, _qubit_term_PMZ_2, _qubit_term_PMZ_3)
 
         else:
             raise ValueError(f"dissipation format not recognized: {dissipation_format}")
 
+        self._rates = []
+        self._terms = []
+        for rate, term in zip(rates, terms):
+            if rate:
+                self._rates.append(rate)
+                self._terms.append(term)
+
     def __matmul__(self, density_op: np.ndarray) -> np.ndarray:
         num_qubits = log2_int(density_op.size) // 2
-        output = self._rate_1 * sum((self._qubit_term_1(density_op, qubit) for qubit in range(num_qubits)), start=np.array(0))
-        if self._rate_2:
-            output += self._rate_2 * sum(self._qubit_term_2(density_op, qubit) for qubit in range(num_qubits))
-        if self._rate_3:
-            output += self._rate_3 * sum(self._qubit_term_3(density_op, qubit) for qubit in range(num_qubits))
-        return output
+        output = sum((rate * term(density_op, qubit) for rate, term in zip(self._rates, self._terms) for qubit in range(num_qubits)))
+        return output if issubclass(type(output), type(density_op)) else 0 * density_op
 
     def __mul__(self, scalar: float) -> "Dissipator":
-        rates = (scalar * self._rates[0], scalar * self._rates[1], scalar * self._rates[2])
-        return Dissipator(rates, self._format)
+        bare_rates = (
+            scalar * self._bare_rates[0],
+            scalar * self._bare_rates[1],
+            scalar * self._bare_rates[2],
+        )
+        return Dissipator(bare_rates, self._format)
 
     def __rmul__(self, scalar: float) -> "Dissipator":
         return self * scalar
