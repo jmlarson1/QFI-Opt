19843fd20dd243a433262f5251945077d0efa029
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-22 17:25:18 -0500

    update MINQ message

--- qfi_opt/examples/benchmark_opt_methods.py
+++ qfi_opt/examples/benchmark_opt_methods.py
@@ -16,6 +16,14 @@ from qfi_opt.examples.calculate_qfi import compute_eigendecompotion, compute_QFI
 
 root_dir = os.path.dirname(os.path.dirname(qfi_opt.__file__))
 minq5_path = os.path.join(root_dir, "MINQ", "py", "minq5")
+if not os.path.isdir(minq5_path):
+    messages = [
+        "Please install (or symlink) MINQ in the QFI-Opt project directory.",
+        "You can do this with:",
+        f"git clone git@github.com:POptUS/MINQ.git {root_dir}/MINQ",
+    ]
+    exit("\n".join(messages))
+sys.path.append(minq5_path)
 
 # sys.path.append("../orbit/py")
 # from orbit4py import ORBIT2
