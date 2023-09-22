3306313b7eb8815f251e142bafc13ece8ffb0844
Michael A. Perlin <mika.perlin@gmail.com>
2023-09-22 15:21:43 -0500

    merge dissipation files

--- qfi_opt/dissipation.py
+++ qfi_opt/dissipation.py
@@ -1,4 +1,10 @@
-import jax.numpy as np
+import typing
+
+import numpy as np
+import jax.numpy as jnp
+
+
+ARRAY_TYPE = typing.TypeVar("ARRAY_TYPE", np.ndarray, jnp.ndarray)
 
 
 class Dissipator:
@@ -60,10 +66,9 @@ class Dissipator:
                 self._rates.append(rate)
                 self._terms.append(term)
 
-    def __matmul__(self, density_op: np.ndarray) -> np.ndarray:
+    def __matmul__(self, density_op: ARRAY_TYPE) -> ARRAY_TYPE:
         num_qubits = log2_int(density_op.size) // 2
-        output = sum((rate * term(density_op, qubit) for rate, term in zip(self._rates, self._terms) for qubit in range(num_qubits)))
-        return output if issubclass(type(output), type(density_op)) else 0 * density_op
+        return sum((rate * term(density_op, num_qubits, qubit) for rate, term in zip(self._rates, self._terms) for qubit in range(num_qubits)))
 
     def __mul__(self, scalar: float) -> "Dissipator":
         bare_rates = (
@@ -102,104 +107,129 @@ matrix get rearranged.
 """
 
 
-def _qubit_term_XYZ_1(density_op: np.ndarray, qubit: int) -> np.ndarray:
+def _qubit_term_XYZ_1(density_op: ARRAY_TYPE, num_qubits: int, qubit: int) -> ARRAY_TYPE:
     """
     Starting with the matrix [[a, b]]  return the matrix [[ 0, -b]
                               [c, d]],                   [[-c,  0]].
     """
     input_shape = density_op.shape
-    num_qubits = log2_int(density_op.size) // 2
     dim_a = 2**qubit
     dim_b = 2 ** (num_qubits - qubit - 1)
     density_op = density_op.reshape((dim_a, 2, dim_b, dim_a, 2, dim_b))
-    output = np.empty_like(density_op)
-    output = output.at[:, 0, :, :, 0, :].set(0)
-    output = output.at[:, 0, :, :, 1, :].set(-density_op[:, 0, :, :, 1, :])
-    output = output.at[:, 1, :, :, 0, :].set(-density_op[:, 1, :, :, 0, :])
-    output = output.at[:, 1, :, :, 1, :].set(0)
+    if isinstance(density_op, jnp.ndarray):
+        output = jnp.empty_like(density_op)
+        output = output.at[:, 0, :, :, 0, :].set(0)
+        output = output.at[:, 0, :, :, 1, :].set(-density_op[:, 0, :, :, 1, :])
+        output = output.at[:, 1, :, :, 0, :].set(-density_op[:, 1, :, :, 0, :])
+        output = output.at[:, 1, :, :, 1, :].set(0)
+    else:
+        output = np.empty_like(density_op)
+        output[:, 0, :, :, 0, :] = 0
+        output[:, 0, :, :, 1, :] = -density_op[:, 0, :, :, 1, :]
+        output[:, 1, :, :, 0, :] = -density_op[:, 1, :, :, 0, :]
+        output[:, 1, :, :, 1, :] = 0
     density_op = density_op.reshape(input_shape)
-    output = output.reshape(input_shape)
-    return output
+    return output.reshape(input_shape)
 
 
-def _qubit_term_XYZ_2(density_op: np.ndarray, qubit: int) -> np.ndarray:
+def _qubit_term_XYZ_2(density_op: ARRAY_TYPE, num_qubits: int, qubit: int) -> ARRAY_TYPE:
     """
     Starting with the matrix [[a, b]]  return the matrix [[d-a,  0 ]
                               [c, d]],                   [[ 0,  a-d]].
     """
     input_shape = density_op.shape
-    num_qubits = log2_int(density_op.size) // 2
     dim_a = 2**qubit
     dim_b = 2 ** (num_qubits - qubit - 1)
     density_op = density_op.reshape((dim_a, 2, dim_b, dim_a, 2, dim_b))
-    output = np.empty_like(density_op)
-    output = output.at[:, 0, :, :, 0, :].set(density_op[:, 1, :, :, 1, :] - density_op[:, 0, :, :, 0, :])
-    output = output.at[:, 0, :, :, 1, :].set(0)
-    output = output.at[:, 1, :, :, 0, :].set(0)
-    output = output.at[:, 1, :, :, 1, :].set(-output[:, 0, :, :, 0, :])
+    if isinstance(density_op, jnp.ndarray):
+        output = jnp.empty_like(density_op)
+        output = output.at[:, 0, :, :, 0, :].set(density_op[:, 1, :, :, 1, :] - density_op[:, 0, :, :, 0, :])
+        output = output.at[:, 0, :, :, 1, :].set(0)
+        output = output.at[:, 1, :, :, 0, :].set(0)
+        output = output.at[:, 1, :, :, 1, :].set(-output[:, 0, :, :, 0, :])
+    else:
+        output = np.empty_like(density_op)
+        output[:, 0, :, :, 0, :] = density_op[:, 1, :, :, 1, :] - density_op[:, 0, :, :, 0, :]
+        output[:, 0, :, :, 1, :] = 0
+        output[:, 1, :, :, 0, :] = 0
+        output[:, 1, :, :, 1, :] = -output[:, 0, :, :, 0, :]
     density_op = density_op.reshape(input_shape)
-    output = output.reshape(input_shape)
-    return output
+    return output.reshape(input_shape)
 
 
-def _qubit_term_XYZ_3(density_op: np.ndarray, qubit: int) -> np.ndarray:
+def _qubit_term_XYZ_3(density_op: ARRAY_TYPE, num_qubits: int, qubit: int) -> ARRAY_TYPE:
     """
     Starting with the matrix [[a, b]]  return the matrix [[0, c]
                               [c, d]],                   [[b, 0]].
     """
     input_shape = density_op.shape
-    num_qubits = log2_int(density_op.size) // 2
     dim_a = 2**qubit
     dim_b = 2 ** (num_qubits - qubit - 1)
     density_op = density_op.reshape((dim_a, 2, dim_b, dim_a, 2, dim_b))
-    output = np.empty_like(density_op)
-    output = output.at[:, 0, :, :, 0, :].set(0)
-    output = output.at[:, 0, :, :, 1, :].set(density_op[:, 1, :, :, 0, :])
-    output = output.at[:, 1, :, :, 0, :].set(density_op[:, 0, :, :, 1, :])
-    output = output.at[:, 1, :, :, 1, :].set(0)
+    if isinstance(density_op, jnp.ndarray):
+        output = jnp.empty_like(density_op)
+        output = output.at[:, 0, :, :, 0, :].set(0)
+        output = output.at[:, 0, :, :, 1, :].set(density_op[:, 1, :, :, 0, :])
+        output = output.at[:, 1, :, :, 0, :].set(density_op[:, 0, :, :, 1, :])
+        output = output.at[:, 1, :, :, 1, :].set(0)
+    else:
+        output = np.empty_like(density_op)
+        output[:, 0, :, :, 0, :] = 0
+        output[:, 0, :, :, 1, :] = density_op[:, 1, :, :, 0, :]
+        output[:, 1, :, :, 0, :] = density_op[:, 0, :, :, 1, :]
+        output[:, 1, :, :, 1, :] = 0
     density_op = density_op.reshape(input_shape)
-    output = output.reshape(input_shape)
-    return output
+    return output.reshape(input_shape)
 
 
 _qubit_term_PMZ_1 = _qubit_term_XYZ_1
 
 
-def _qubit_term_PMZ_2(density_op: np.ndarray, qubit: int) -> np.ndarray:
+def _qubit_term_PMZ_2(density_op: ARRAY_TYPE, num_qubits: int, qubit: int) -> ARRAY_TYPE:
     """
     Starting with the matrix [[a, b]]  return the matrix [[ d,  0]
                               [c, d]],                   [[ 0, -d]].
     """
     input_shape = density_op.shape
-    num_qubits = log2_int(density_op.size) // 2
     dim_a = 2**qubit
     dim_b = 2 ** (num_qubits - qubit - 1)
     density_op = density_op.reshape((dim_a, 2, dim_b, dim_a, 2, dim_b))
-    output = np.empty_like(density_op)
-    output = output.at[:, 0, :, :, 0, :].set(density_op[:, 1, :, :, 1, :])
-    output = output.at[:, 0, :, :, 1, :].set(0)
-    output = output.at[:, 1, :, :, 0, :].set(0)
-    output = output.at[:, 1, :, :, 1, :].set(-density_op[:, 1, :, :, 1, :])
+    if isinstance(density_op, jnp.ndarray):
+        output = jnp.empty_like(density_op)
+        output = output.at[:, 0, :, :, 0, :].set(density_op[:, 1, :, :, 1, :])
+        output = output.at[:, 0, :, :, 1, :].set(0)
+        output = output.at[:, 1, :, :, 0, :].set(0)
+        output = output.at[:, 1, :, :, 1, :].set(-density_op[:, 1, :, :, 1, :])
+    else:
+        output = np.empty_like(density_op)
+        output[:, 0, :, :, 0, :] = density_op[:, 1, :, :, 1, :]
+        output[:, 0, :, :, 1, :] = 0
+        output[:, 1, :, :, 0, :] = 0
+        output[:, 1, :, :, 1, :] = -density_op[:, 1, :, :, 1, :]
     density_op = density_op.reshape(input_shape)
-    output = output.reshape(input_shape)
-    return output
+    return output.reshape(input_shape)
 
 
-def _qubit_term_PMZ_3(density_op: np.ndarray, qubit: int) -> np.ndarray:
+def _qubit_term_PMZ_3(density_op: ARRAY_TYPE, num_qubits: int, qubit: int) -> ARRAY_TYPE:
     """
     Starting with the matrix [[a, b]]  return the matrix [[-a,  0]
                               [c, d]],                   [[ 0,  a]].
     """
     input_shape = density_op.shape
-    num_qubits = log2_int(density_op.size) // 2
     dim_a = 2**qubit
     dim_b = 2 ** (num_qubits - qubit - 1)
     density_op = density_op.reshape((dim_a, 2, dim_b, dim_a, 2, dim_b))
-    output = np.empty_like(density_op)
-    output = output.at[:, 0, :, :, 0, :].set(-density_op[:, 0, :, :, 0, :])
-    output = output.at[:, 0, :, :, 1, :].set(0)
-    output = output.at[:, 1, :, :, 0, :].set(0)
-    output = output.at[:, 1, :, :, 1, :].set(density_op[:, 0, :, :, 0, :])
+    if isinstance(density_op, jnp.ndarray):
+        output = jnp.empty_like(density_op)
+        output = output.at[:, 0, :, :, 0, :].set(-density_op[:, 0, :, :, 0, :])
+        output = output.at[:, 0, :, :, 1, :].set(0)
+        output = output.at[:, 1, :, :, 0, :].set(0)
+        output = output.at[:, 1, :, :, 1, :].set(density_op[:, 0, :, :, 0, :])
+    else:
+        output = np.empty_like(density_op)
+        output[:, 0, :, :, 0, :] = -density_op[:, 0, :, :, 0, :]
+        output[:, 0, :, :, 1, :] = 0
+        output[:, 1, :, :, 0, :] = 0
+        output[:, 1, :, :, 1, :] = density_op[:, 0, :, :, 0, :]
     density_op = density_op.reshape(input_shape)
-    output = output.reshape(input_shape)
-    return output
+    return output.reshape(input_shape)
