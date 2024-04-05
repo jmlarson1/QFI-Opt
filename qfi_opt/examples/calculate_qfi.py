817658ea98e638c98ec710a19a1238ae70f0213f
Jeffrey Larson <jmlarson@anl.gov>
2024-04-05 11:27:54 -0500

    simpler api for now

--- qfi_opt/examples/calculate_qfi.py
+++ qfi_opt/examples/calculate_qfi.py
@@ -15,6 +15,73 @@ def compute_eigendecomposition(rho: np.ndarray):
     eigvecs = eigvecs.T  # make the k-th eigenvector eigvecs[k, :] = eigvecs[k]
     return eigvals, eigvecs
 
+def compute_QFI_simpler_api(
+    eigvals: np.ndarray, eigvecs: np.ndarray, A: np.ndarray, params:
+    np.ndarray, grad, get_jacobian, obj_params, tol: float = 1e-8, etol_scale:
+    float = 10
+) -> float:
+    # Note: The eigenvectors must be rows of eigvecs
+    num_vals = len(eigvals)
+    num_params = len(params)
+
+    G = obj_params["G"]
+
+    # There should never be negative eigenvalues, so their magnitude gives an
+    # empirical estimate of the numerical accuracy of the eigendecomposition.
+    # We discard any QFI terms denominators within an order of magnitude of
+    # this value.
+    tol = max(tol, -etol_scale * np.min(eigvals))
+
+    # Compute QFI and grad
+    running_sum = 0
+
+    if grad.size > 0:
+
+        dA = get_jacobian(params, obj_params["N"], dissipation_rates=obj_params["dissipation"])
+        dA = np.transpose(dA, (2, 0, 1))
+
+        grad[:] = np.zeros(num_params)
+        lambda_grads = np.zeros((num_params, num_vals))
+        psi_grads = np.zeros((num_params, num_vals, num_vals), dtype="cdouble")
+
+        for k in range(num_params):
+            # compute gradients of each eigenvalue
+            lambda_grad_k, psi_grad_k = get_matrix_grads_sylvester(dA[k], eigvals, eigvecs, tol)
+            lambda_grads[k] = lambda_grad_k
+            psi_grads[k] = psi_grad_k
+
+    for i in range(num_vals):
+        for j in range(i + 1, num_vals):
+            denom = eigvals[i] + eigvals[j]
+            diff = eigvals[i] - eigvals[j]
+            if not np.isclose(denom, 0, atol=tol, rtol=tol) and not np.isclose(diff, 0, atol=tol, rtol=tol):
+                numer = diff**2
+                term = eigvecs[i].conj() @ G @ eigvecs[j]
+                quotient = numer / denom
+                squared_modulus = np.absolute(term) ** 2
+                running_sum += quotient * squared_modulus
+                if grad.size > 0:
+                    for k in range(num_params):
+                        # fill in gradient
+                        grad[k] += kth_partial_derivative(
+                            quotient,
+                            squared_modulus,
+                            eigvals[i],
+                            eigvals[j],
+                            lambda_grads[k, i],
+                            lambda_grads[k, j],
+                            eigvecs[i],
+                            eigvecs[j],
+                            psi_grads[k, i],
+                            psi_grads[k, j],
+                            G,
+                        )
+
+    if grad.size > 0:
+        return 4 * running_sum, 4 * grad
+    else:
+        return 4 * running_sum, []
+
 
 def compute_QFI(eigvals: np.ndarray, eigvecs: np.ndarray, G: np.ndarray, A:
         np.ndarray= np.empty(0), dA: np.ndarray= np.empty(0), d2A: np.ndarray = np.empty(0),
